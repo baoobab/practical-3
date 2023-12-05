6324b6253d1e5cc466ddbb27a04186798f351a08
baoobab <v.xxl.90@mail.ru>
2023-12-05 16:32:13 +0300

    fixes

--- main.cpp
+++ main.cpp
@@ -90,6 +90,8 @@ int main() {
 
                     k++;
                 }
+
+                cout << "\n" << "1.B)" << "\n";
                 break;
             }
             case 2: {
@@ -134,95 +136,3 @@ int main() {
     
     return 0;
 }
-
-
-
-// int main() {
-//     const int N = 3;
-//     const int arrSize = N * N;
-//     // int arr[arrSize] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-//     int arr[arrSize] = {};
-//     int cnt = 1;
-//     int sizer = N;
-
-//     // cout << arr[ N - 1] << " " << arr[ arrSize - 1];
-//     // cin >> cnt;
-//     int test;
-    // int *leftBorder = arr; 
-    // int *rightBorder = &arr[N - 1];
-    // int *topBorder = &arr[2 * N - 1];
-    // int *bottomBorder = &arr[arrSize - 1];
-//     int *pleftBorder = arr; 
-//     int *prightBorder = &arr[N - 1];
-//     int *ptopBorder = &arr[2 * N - 1];
-//     int *pbottomBorder = &arr[arrSize - 1];
-
-//     bool horizontalReverse = false;
-//     bool verticalReverse = false;
-
-//     while (cnt <= arrSize) {
-//         // горизонтальный цикл
-//         if (horizontalReverse) {
-            // for (int *next = rightBorder, *end = leftBorder; next <= end; next--) {
-            //     // *next = getRandomValueFromRange(1, arrSize);
-            //     *next = cnt;
-            //     cnt++;
-            // }  
-//             horizontalReverse = false;  
-//             leftBorder = leftBorder - (sizer - 2);
-//             rightBorder = rightBorder + (sizer - 2);
-            
-//             prightBorder = rightBorder;
-//             pleftBorder = leftBorder;
-//         } else {
-//             for (int *next = leftBorder, *end = rightBorder; next <= end; next++) {
-//                 // *next = getRandomValueFromRange(1, arrSize);
-//                 *next = cnt;
-//                 cnt++;
-//             }   
-//             horizontalReverse = true;  
-//             leftBorder = leftBorder + 1 + (N - (sizer - 1));
-//             rightBorder = rightBorder + (sizer - 3);
-
-//             prightBorder = rightBorder;
-//             pleftBorder = leftBorder; 
-//         }
-    
-//         arrPrint(arr, N);
-//         cin >> test;
-
-//         // вертикальный цикл
-//         if (verticalReverse) {
-//             for (int *next = bottomBorder, *end = topBorder; next <= end; next -= sizer) {
-//                 // *next = getRandomValueFromRange(1, arrSize);
-//                 *next = cnt;
-//                 cnt++;
-//             }    
-//             verticalReverse = false;
-//             topBorder = topBorder + sizer - 1;
-//             bottomBorder = bottomBorder + sizer - 1;
-
-//             ptopBorder = topBorder;
-//             pbottomBorder = bottomBorder;
-//         } else {
-            // for (int *next = topBorder, *end = bottomBorder; next <= end; next += sizer) {
-            //     // *next = getRandomValueFromRange(1, arrSize);
-            //     *next = cnt;
-            //     cnt++;
-            // }      
-//             verticalReverse = false;
-//             topBorder = leftBorder + sizer;
-//             bottomBorder = bottomBorder - (sizer + 1) * 2; 
-
-//             ptopBorder = topBorder;
-//             pbottomBorder = bottomBorder;
-//         }
-        
-//         arrPrint(arr, N);
-//         cin >> test;
-//         sizer--;
-
-//     }
-
-//     return 0;
-// }
